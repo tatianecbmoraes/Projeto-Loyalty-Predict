db726c78099e0a10ae168fb02f3e0e92dee4e326
Tatiane Moraes <tatianecbmoraes@gmail.com>
2025-10-16 11:35:31 -0300

    Atualiza fs_transacional.sql com novas métricas

--- src/analytics/fs_transacional.sql
+++ src/analytics/fs_transacional.sql
@@ -42,12 +42,14 @@ FROM tb_transacao
 
 GROUP BY idCliente
 
-)
+),
+
+tb_agg_calc AS (
 
 SELECT 
         *,
  coalesce(1. * qtdeAtivacaoVida / qtdeTransacaoVida,0) AS qtdeTransacaoDiaVida,
- coalesce(1. * qtdeTransacaoD7  / qtdeAtivacaoD7,0)  AS qtdeTransacaoDiaD7, 
+ coalesce(1. * qtdeTransacaoD7 / qtdeAtivacaoD7,0)  AS qtdeTransacaoDiaD7, 
  coalesce(1. * qtdeTransacaoD14 / qtdeAtivacaoD14,0) AS qtdeTransacaoDiaD14,
  coalesce(1. * qtdeTransacaoD28 / qtdeAtivacaoD28,0) AS qtdeTransacaoDiaD28,
  coalesce(1. * qtdeTransacaoD56 / qtdeAtivacaoD56,0) AS qtdeTransacaoDiaD56,
@@ -55,3 +57,64 @@ SELECT
 coalesce(1. * qtdeAtivacaoD28 / 28,0) AS pcAtivacaoMau
 
 FROM tb_agg_transacao
+),
+
+tb_horas_dia AS (
+
+SELECT idCliente,
+        dtDia,
+        24 * (max(julianday(DtCriacao)) - min(julianday(DtCriacao))) AS duracao
+
+FROM tb_transacao
+
+GROUP BY idCliente, dtDia
+),
+tb_hora_cliente as (
+
+SELECT idCliente,
+       sum(duracao) AS qtHorasVida,
+       sum(CASE WHEN dtDia >= date ('2025-10-01', '-7 day') THEN duracao ELSE 0 END) AS qtdeHorasD7,
+       sum(CASE WHEN dtDia >= date ('2025-10-01', '-14 day') THEN duracao ELSE 0 END) AS qtdeHorasD14,
+       sum(CASE WHEN dtDia >= date ('2025-10-01', '-28 day') THEN duracao ELSE 0 END) AS qtdeHorasD28,
+       sum(CASE WHEN dtDia >= date ('2025-10-01', '-56 day') THEN duracao ELSE 0 END) AS qtdeHorasD56
+
+FROM tb_horas_dia
+
+GROUP BY idCliente
+),
+
+tb_lag_dia AS (
+
+SELECT idCliente,
+       dtDia,
+       LAG(dtDia) OVER (PARTITION BY idCliente ORDER BY dtDia ) AS lagDia
+
+FROM tb_horas_dia 
+),
+
+tb_intervalo_dias AS (
+
+SELECT idCliente,
+       avg(julianday(dtDia) - julianday(lagDia)) AS avgIntervaloDias,
+       avg(CASE WHEN dtDia >= date('2025-10-01', '- 28day') THEN julianday(dtDia) - julianday(lagDia) END) AS avgIntervaloDias28
+
+FROM tb_lag_dia
+
+GROUP BY idCliente
+)
+SELECT t1.*,
+        t2.qtdeHorasD7,
+        t2.qtdeHorasD14,
+        t2.qtdeHorasD28,
+        t2.qtdeHorasD56,
+        avgIntervaloDias,
+        avgIntervaloDias28
+
+
+FROM tb_agg_calc as t1
+
+LEFT JOIN tb_hora_cliente as t2
+ON t1.idCliente = t2.idCliente
+
+LEFT JOIN tb_intervalo_dias  AS t3
+ON t1.idCliente = t3.idCliente
